57cc002d3228b13b5b9b00c5e6a3937d2631512e
weklihua <wek.lihua@gmail.com>
2022-06-07 15:24:27 -0700

    change css

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 const MAX_GUESSES = 6
 const ALLOWED_LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 const GAME_WORD_BANK = ['alligator', 'bear', 'cheetah', 'deer', 'elephant', 'fox', 'giraffe',
-'hippo', 'jaguar', 'kangaroo', 'lion', 'monkey', 'otter', 'penguin', 'rabbit', 'shark', 'tiger', 'wolf', 'yak', 'zebra', 'bee']
+'hippo', 'jaguar', 'kangaroo', 'lion', 'monkey', 'otter', 'penguin', 'rabbit', 'shark', 'tiger', 'wolf', 'yak', 'zebra', 'bee', 'octopus']
 
 /*----- app's state (variables) -----*/
 let chosenLetters, currentNumOfGuesses, secretWord, currentGuess, totalLine
@@ -38,7 +38,7 @@ function init(){
     win = null
     //guessesLeftedEl.innerText = "Number of Guesses Left: 6" 
     imageEl.setAttribute("src", "https://github.com/weklihua/Hangman/blob/main/image/0.jpeg?raw=true")
-    messageEl.innerText = "Make a guess"
+    messageEl.innerText = "Guess what animal I am!"
     document.getElementById('alert').setAttribute('class', "alert alert-info")
     messageEl.style.color = 'rgb(98, 93, 93)'
 
@@ -69,13 +69,13 @@ function isWinning(){
     if (win === true){
         //console.log('Congratulations!')
         messageEl.innerText = 'Congratulations, you won!'
-        messageEl.style.color = 'black'
+        //messageEl.style.color = 'black'
         document.getElementById('alert').setAttribute('class', "alert alert-success")
         keyboard.removeEventListener('click', handleClick)
     } else if (win === false){
         //console.log('loser!')
         messageEl.innerText = 'You lost, try again!'
-        messageEl.style.color = 'black'
+        //messageEl.style.color = 'black'
         document.getElementById('alert').setAttribute('class', "alert alert-danger")
         keyboard.removeEventListener('click', handleClick)
     } else if (win === null) {
